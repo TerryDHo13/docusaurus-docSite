986a56f9e9162554cad5b3889e3fe04839261532
Terry Dershyan Ho <terrydho@gmail.com>
2024-05-02 10:47:37 +0800

    updated commit, added searchbar

---
+++ my-website/src/components/HomepageFeatures/index.js
@@ -0,0 +1,61 @@
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+
+const FeatureList = [
+  {
+    title: 'Testing Purposes 1',
+    Svg: require('@site/static/img/brown_book_blue_bg.svg').default,
+    description: (
+      <>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+      </>
+    ),
+  },
+  {
+    title: 'Testing Purposes 2',
+    Svg: require('@site/static/img/brown_book_blue_bg.svg').default,
+    description: (
+      <>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+      </>
+    ),
+  },
+  {
+    title: 'testing Purposes 3',
+    Svg: require('@site/static/img/brown_book_blue_bg.svg').default,
+    description: (
+      <>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+      </>
+    ),
+  },
+];
+
+function Feature({Svg, title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3">{title}</Heading>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
